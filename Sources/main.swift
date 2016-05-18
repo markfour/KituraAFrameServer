c5fd1ac5aff20ab49905f8f07da06bd296a10343
markfour <axhfaro@gmail.com>
2016-05-18 15:25:31 +0900

    Add html

--- Sources/main.swift
+++ Sources/main.swift
@@ -4,9 +4,19 @@ import KituraSys
 
 let router = Router()
 
+let html = "<!DOCTYPE html><head><script src=\"https://aframe.io/releases/latest/aframe.min.js\"></script></head><body><a-scene><a-cube position=\"-2 1 0\" color=\"#C00\"></a-cube><a-sphere position=\"0 1 0\" color=\"#0C0\"></a-sphere><a-cylinder position=\"2 1 0\" color=\"#00C\"></a-cylinder><a-sky color=\"#EEE\"></a-sky></a-scene></body>"
+
 router.get("/") {
 request, response, next in
-    response.status(.OK).send("Hello, World!")
+    let responseData = "response"
+    response.status(.OK).send(html)
+    next()
+}
+
+router.get("other") {
+request, response, next in
+    let responseData = "other"
+    response.status(.OK).send(responseData)
     next()
 }
 
