53c7f067028ca4bd634c71ad161ec2285136488d
markfour <axhfaro@gmail.com>
2016-05-19 10:33:03 +0900

    Add Swift 3.0 function

--- Sources/a-frame.swift
+++ Sources/a-frame.swift
@@ -1,4 +1,24 @@
-public struct Book {
-  var title: String = "title"
-  var author: String = "author"
+import SwiftyJSON
+
+struct htmlScheme {
+  static let doctype = "<!DOCTYPE html>"
+  static let htmlStart = "<html>"
+  static let htmlEnd = "</html>"
+  static let headStart = "<head>"
+  static let head = "<script src=\"https://aframe.io/releases/latest/aframe.min.js\">"
+  static let headEnd = "</head>"
+  static let aSenceStart = "<a-scene>"
+//  static let a
 }
+
+struct Aframe {
+  var sample: String {
+    get {
+      return ""
+    }
+  }
+  var title: String?
+  var body = ""
+}
+
+
--- Sources/main.swift
+++ Sources/main.swift
@@ -1,3 +1,4 @@
+import Foundation
 import Kitura
 import KituraNet
 import KituraSys
@@ -6,48 +7,75 @@ let router = Router()
 
 let html = "<!DOCTYPE html><head><script src=\"https://aframe.io/releases/latest/aframe.min.js\"></script></head><body><a-scene><a-cube position=\"-2 1 0\" color=\"#C00\"></a-cube><a-sphere position=\"0 1 0\" color=\"#0C0\"></a-sphere><a-cylinder position=\"2 1 0\" color=\"#00C\"></a-cylinder><a-sky color=\"#EEE\"></a-sky></a-scene></body>"
 
-struct htmlScheme {
-  static let doctype = "<!DOCTYPE html>"
-  static let htmlStart = "<html>"
-  static let htmlEnd = "</html>"
-  static let headStart = "<head>"
-  static let headEnd = "</head>"
+
+
+func constructResourcePathFromSourceLocation(resource: String) -> String {
+  let fileName = NSString(string: #file)
+  let resourceFilePrefixRange: NSRange
+  let lastSlash = fileName.range(of: "/", options: NSStringCompareOptions.backwardsSearch)
+  if  lastSlash.location != NSNotFound  {
+    resourceFilePrefixRange = NSMakeRange(0, lastSlash.location+1)
+  } else {
+    resourceFilePrefixRange = NSMakeRange(0, fileName.length)
+  }
+  return fileName.substring(with: resourceFilePrefixRange) + "resources/" + resource
 }
 
-struct Aframe {
-  static
+class test {
+  func test(t: String) -> String {
+    return "tester" + t
+  }
   
-  var sample: String {
-    get {
-      return ""
-    }
+  func nopram() {
+    
   }
-  var title: String?
-  var body = ""
 }
 
-class testClass {
-  func getText() -> String {
-    return "myString"
-  }
+func noParam() {
+  
 }
+
+func paramater(_ i :Int) {
+  
+}
+
+noParam()
+paramater(1)
+
+let t = test()
+
+//t.test("s")
 
 router.get("/") {
-request, response, next in
-//    let html = testClass.myString()
-    let b = Book()
-    let aframe = Aframe()
-    let responseData = "response"
-    response.status(.OK).send(b.title)
-    next()
+  request, response, next in
+  let aframe = Aframe()
+  let responseData = "response"
+  response.status(.OK).send(html)
+  next()
 }
 
 router.get("other") {
-request, response, next in
-    let responseData = "other"
-    response.status(.OK).send(responseData)
-    next()
+  request, response, next in
+  let responseData = "other"
+  //    response.status(.OK).send(responseData)
+  response.status(.OK).send(html)
+  next()
 }
+
+let htmlFilePath = "index.path"
+#if os(Linux)
+let fileManager = NSFileManager.defaultManager()
+#else
+let fileManager = NSFileManager.default()
+#endif
+//let potentialResource = constructResourcePathFromSourceLocation("abs")
+//let fileExists = fileManager.fileExists(atPath: potentialResource)
+//if fileExists {
+//  print("get file")
+//} else {
+//  print("no file path")
+//}
+//print(resourceFileName)
 
 let server = HTTPServer.listen(port: 8090, delegate: router)
 Server.run()
